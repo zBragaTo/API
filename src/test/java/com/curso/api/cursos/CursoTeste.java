e33be1dc72a8fefd0577c8030fd222ced86fd551
Leonardo Bragato Bassi <leonardobragatobassi@hotmail.com>
2023-12-13 19:03:00 -0300

    Explorando os asserts

--- src/test/java/com/curso/api/cursos/CursoTeste.java
+++ src/test/java/com/curso/api/cursos/CursoTeste.java
@@ -2,28 +2,46 @@ package com.curso.api.cursos;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.curso.api.entities.Curso;
 
 class CursoTeste {
-	
+
 	@Test
 	void test() {
 //		fail("Not yet implemented");
 	}
-	
+
 	@Test
 	void testaCursosNaoVazio() {
-		
+
 		Curso c = new Curso();
 		assertNotNull(c);
 	}
-	
+
 	@Test
 	void valorDoCursoNaoNulo() {
+		
 		Curso c = new Curso();
-		assertNotNull(c.getValorDoCurso());
+		Assertions.assertNotNull(c.getValorDoCurso());
+	}
+
+	@Test
+	void testaNovoCursoValorZero() {
+
+		Curso curso = new Curso();
+		Assertions.assertEquals(new BigDecimal(0), curso.getValorDoCurso());
+	}
+
+	@Test
+	void testaNovoCursoSemAlunos() {
+
+		Curso curso = new Curso();
+		Assertions.assertTrue(curso.getAlunos().isEmpty());
 	}
 
 }
